4fb882e065503e648b6538230013668c41a0c323
Josh Elison <joshelison@gmail.com>
2019-09-25 13:10:34 -0600

    finnished the core requirements

--- web/Week02Team/W02Java.js
+++ web/Week02Team/W02Java.js
@@ -1,3 +1,8 @@
 function clicked(){
   alert("CLICKED!!");
 }
+
+function changeBackground(){
+    color=document.getElementById("color-box").value;
+    document.getElementById("first").style.background = color;
+}
