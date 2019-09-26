e4d0166e2b991f77fdb5d9d2312005a5b6782040
Josh Elison <joshelison@gmail.com>
2019-09-26 12:27:49 -0600

    Changed the homepage to a php file

---
+++ web/homepage/homepage.php
@@ -0,0 +1,34 @@
+<?php 
+function getTime(){
+    date_default_timezone_set('America/Boise');
+
+    $current_date = date('d/m/Y == H:i:s');
+    echo $current_date;
+}  
+?>
+
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+  <meta charset="UTF-8">
+  <title>Josh Elison's Homepage</title>
+  <link rel="stylesheet" type="text/css" href="homepageSS.css">
+  <script src="homepageJava.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <h1>
+        Josh Elison
+    </h1>
+    <div class="navbar">
+        <a>Home</a>
+        <a href="assignments.html">Assignments</a>
+        <a href="intro.html">About Me</a>
+        <a href="https://github.com/jelison4/cs313-php">My GitHub Page for CS313</a>
+    </div>
+    
+    <p onload="getTime()">
+    </p>
+
+</body>
+</html>
