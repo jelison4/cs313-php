4c5d033eb7e23c7ea7261b679b0786041402d55f
Josh Elison <joshelison@gmail.com>
2019-10-01 23:07:09 -0600

    Added cart link and other formating

---
+++ web/Week03/cart.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+  <meta charset="UTF-8">
+  <title>Week 03: Shopping Cart</title>
+  <link rel="stylesheet" type="text/css" href="W03SS.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<p>
+    it worked
+</p>
+</body>
+</html>
