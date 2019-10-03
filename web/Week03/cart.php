0c442d8f1844dd0d6d575a73ccba358b977c3064
Josh Elison <joshelison@gmail.com>
2019-10-02 18:12:19 -0600

    formated money in cart table

--- web/Week03/cart.php
+++ web/Week03/cart.php
@@ -39,7 +39,7 @@
 
      function display(){
       setlocale(LC_MONETARY, 'en_US');
-      echo "<tr><td>" . $this->getTitle() . "</td><td>" . money_format('%i', $this->getPrice()) . "</td></tr>";
+      echo "<tr><td>" . $this->getTitle() . "</td><td class='money'>$" . money_format('%i', $this->getPrice()) . "</td></tr>";
      }
    }
 ?>
