35d4c6f6e932828b943bf3a9f148a683a600b62e
Josh Elison <joshelison@gmail.com>
2019-10-03 16:09:00 -0600

    intial checkout formating

--- web/Week03/checkout.php
+++ web/Week03/checkout.php
@@ -1,3 +1,16 @@
 <?php
    
 ?>
+
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+  <meta charset="UTF-8">
+  <title>Week 03: Shopping Cart</title>
+  <link rel="stylesheet" type="text/css" href="W03SS.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <h1>Checkout</h1>
+</body>
+</html>
