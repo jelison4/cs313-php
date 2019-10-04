68318758c4b993530b7146105374cf10168507e1
Josh Elison <joshelison@gmail.com>
2019-10-04 11:19:02 -0600

    fixing auto caps

--- web/Week03/checkout.php
+++ web/Week03/checkout.php
@@ -16,7 +16,7 @@
   <form action="confirmation.php" method="POST">
       <table>
         <tr><td>Address:</td><td><input type="text" id="address"></td></tr>
-        <tr><td>City:</td><td><input type="text" id="city" maxlength="2" onkeyup="this.value = this.value.toUpperCase();"></td></tr>
+        <tr><td>City:</td><td><input type="text" id="city" maxlength="2" style="text-transform: uppercase;"></td></tr>
         <tr><td>State:</td><td><input type="text" id="State"></td></tr>
         <tr><td>Zip Code:</td><td><input type="text" id="zip"></td></tr>
         <tr><td colspan="2" style="text-align:right"><input type="submit" value="Complete Order"> </td></tr>
