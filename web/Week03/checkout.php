09a09f227e10657f183db562e0915d28f7d02b89
Josh Elison <joshelison@gmail.com>
2019-10-04 11:21:00 -0600

    I was auto capitalizing city rather then state

--- web/Week03/checkout.php
+++ web/Week03/checkout.php
@@ -16,8 +16,8 @@
   <form action="confirmation.php" method="POST">
       <table>
         <tr><td>Address:</td><td><input type="text" id="address"></td></tr>
-        <tr><td>City:</td><td><input type="text" id="city" maxlength="2" style="text-transform: uppercase;"></td></tr>
-        <tr><td>State:</td><td><input type="text" id="State"></td></tr>
+        <tr><td>City:</td><td><input type="text" id="city"></td></tr>
+        <tr><td>State:</td><td><input type="text" id="State" maxlength="2" style="text-transform: uppercase;"></td></tr>
         <tr><td>Zip Code:</td><td><input type="text" id="zip"></td></tr>
         <tr><td colspan="2" style="text-align:right"><input type="submit" value="Complete Order"> </td></tr>
       </table>
