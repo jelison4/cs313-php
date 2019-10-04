528b612f3aff38c50042fcb846ff775c65d9226b
Josh Elison <joshelison@gmail.com>
2019-10-04 11:06:20 -0600

    added complete order button

--- web/Week03/checkout.php
+++ web/Week03/checkout.php
@@ -13,12 +13,13 @@
 <body>
   <h1>Checkout</h1>
 
-  <form>
+  <form action="confirmation.php">
       <table>
         <tr><td>Address:</td><td><input type="text" id="address"></td></tr>
         <tr><td>City:</td><td><input type="text" id="city"></td></tr>
         <tr><td>State:</td><td><input type="text" id="State"></td></tr>
         <tr><td>Zip Code:</td><td><input type="text" id="zip"></td></tr>
+        <tr><td colspan="2"><input type="submit" value="Complete Order"> </td></tr>
       </table>
   </form>
 </body>
