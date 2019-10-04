4465c3e8f035a51a0a59a1a352b13ba3f6485b02
Josh Elison <joshelison@gmail.com>
2019-10-04 12:24:50 -0600

    really fixed the address this time

--- web/Week03/checkout.php
+++ web/Week03/checkout.php
@@ -16,9 +16,9 @@
   <form action="confirmation.php" method="POST">
       <table>
         <tr><td>Address:</td><td><input type="text" name="address"></td></tr>
-        <tr><td>City:</td><td><input type="text" id="city"></td></tr>
-        <tr><td>State:</td><td><input type="text" id="State" maxlength="2" style="text-transform: uppercase;"></td></tr>
-        <tr><td>Zip Code:</td><td><input type="text" id="zip"></td></tr>
+        <tr><td>City:</td><td><input type="text" name="city"></td></tr>
+        <tr><td>State:</td><td><input type="text" name="State" maxlength="2" style="text-transform: uppercase;"></td></tr>
+        <tr><td>Zip Code:</td><td><input type="text" name="zip"></td></tr>
         <tr><td colspan="2" style="text-align:right"><input type="submit" value="Complete Order"> </td></tr>
       </table>
   </form>
