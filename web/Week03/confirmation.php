452db3506f0d6ec7a897531ff82c63b1da11ec40
Josh Elison <joshelison@gmail.com>
2019-10-04 11:36:27 -0600

    trouble shooting address

--- web/Week03/confirmation.php
+++ web/Week03/confirmation.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  $_SESSION["address"]=$_POST["address"];
+  $_SESSION["address"]=$_GET["address"];
   $_SESSION["city"]=$_POST["city"];
   $_SESSION["State"]=$_POST["State"];
   $_SESSION["zip"]=$_POST["zip"];
