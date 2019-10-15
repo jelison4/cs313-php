a7863e31225035e8f7597ac8ad44867bf4c78d91
josh <joshelison@gmail.com>
2019-10-15 15:35:30 -0600

    Co-authored-by: Burden, Henry <bur15054@byui.edu> Co-authored-by: benjamin93dass <39276385+benjamin93dass@users.noreply.github.com> Co-authored-by: Samuelzinh0 <samuelzinhomoreira@outlook.com>

--- db/week05.sql
+++ db/week05.sql
@@ -1,2 +1,16 @@
-CREATE TABLE "scriptures" ("ID" SERIAL PRIMARY KEY, "book" varchar, "chapter" int, "verse" int, "content" varchar);
+CREATE TABLE "scripture" ("ID" SERIAL PRIMARY KEY, "book" VARCHAR(50) NOT NULL, "chapter" INT NOT NULL, "verse" INT NOT NULL, "content" VARCHAR(666) NOT NULL);
 
+INSERT INTO scripture (book, chapter, verse, content) VALUES ('John', 1, 5, 'And the light shineth in darkness; and the darkness comprehended it not.');
+INSERT INTO scripture (book, chapter, verse, content) VALUES ('Doctrine and Covenants', 88, 49, 'The light shineth in darkness, and the darkness comprehendeth it not; nevertheless, the day shall come when you shall comprehend even God, being quickened in him and by him.');
+INSERT INTO scripture (book, chapter, verse, content) VALUES ('Doctrine and Covenants', 93, 28, 'He that keepeth his commandments receiveth truth and light, until he is glorified in truth and knoweth all things.');
+INSERT INTO scripture (book, chapter, verse, content) VALUES ('Mosiah', 16, 9, 'He is the alight and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.');
+
+
+/*Example*/
+CREATE TABLE person
+(
+	id SERIAL PRIMARY KEY NOT NULL
+	, person_name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO person (person_name) VALUES ('John Doe');
---
+++ weekTeamActivity05.php
@@ -0,0 +1,31 @@
+<?php
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+$statement = $db->query('SELECT book, chapter, verse, content FROM scripture');
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+  echo 'book: ' . $row['book'] . ' chapter: ' . $row['chapter'] . 'verse' . $row['verse'] . 'content' . $row['content'] .'<br/>';
+}
+
+?>
