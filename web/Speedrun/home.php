ad78105c4125cfc10e23f6fc7df7834f7b2b7c41
Josh Elison <joshelison@gmail.com>
2019-10-17 13:00:20 -0600

    made the initial files for speedrunning homepage

---
+++ web/Speedrun/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+  <meta charset="UTF-8">
+  <title>Speed Running!!</title>
+  <link rel="stylesheet" type="text/css" href="speedrunSS.css">
+  <script src="speedrunJava.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+</body>
+</html>
