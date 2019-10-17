6be07eed8e7b7606c8d4203ca89b78902ff9634e
Josh Elison <joshelison@gmail.com>
2019-10-17 12:59:30 -0600

    reformatted database and added data

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,14 +1,14 @@
-CREATE TABLE "users" ("User_ID" SERIAL PRIMARY KEY, "username" varchar, "run_ID" int);
-CREATE TABLE "run" ("ID" SERIAL PRIMARY KEY, "game_ID" int, "platform_ID" int, "time" varchar, "valid" boolean,  "category_id" int);
-CREATE TABLE "game" ("ID" SERIAL PRIMARY KEY, "title" varchar);
-CREATE TABLE "category" ("ID" SERIAL PRIMARY KEY, "category_title" varchar);
-CREATE TABLE "platform" ("ID" SERIAL PRIMARY KEY, "name" varchar);
+CREATE TABLE "users" ("id" SERIAL PRIMARY KEY, "username" varchar);
+CREATE TABLE "run" ("id" SERIAL PRIMARY KEY, "user_id" int, "game_id" int, "platform_id" int, "time" varchar, "valid" boolean,  "category_id" int);
+CREATE TABLE "game" ("id" SERIAL PRIMARY KEY, "title" varchar);
+CREATE TABLE "category" ("id" SERIAL PRIMARY KEY, "category_title" varchar);
+CREATE TABLE "platform" ("id" SERIAL PRIMARY KEY, "name" varchar);
+ALTER TABLE "run" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "run" ADD FOREIGN KEY ("game_id") REFERENCES "game" ("id");
+ALTER TABLE "run" ADD FOREIGN KEY ("platform_id") REFERENCES "platform" ("id");
+ALTER TABLE "run" ADD FOREIGN KEY ("category_id") REFERENCES "category" ("id");
 
-ALTER TABLE "users" ADD FOREIGN KEY ("run_ID") REFERENCES "run" ("ID");
-ALTER TABLE "run" ADD FOREIGN KEY ("game_ID") REFERENCES "game" ("ID");
-ALTER TABLE "run" ADD FOREIGN KEY ("platform_ID") REFERENCES "platform" ("ID");
-ALTER TABLE "game" ADD FOREIGN KEY ("category_id") REFERENCES "category" ("ID");
-
+/* Inserting platforms*/
 INSERT INTO platform (name) VALUES ('PC');
 INSERT INTO platform (name) VALUES ('Playstation 4');
 INSERT INTO platform (name) VALUES ('Xbox One');
@@ -16,6 +16,7 @@ INSERT INTO platform (name) VALUES ('Nintendo Switch');
 INSERT INTO platform (name) VALUES ('Nintendo 64');
 INSERT INTO platform (name) VALUES ('Super Nintendo');
 
+/* Inserting games*/
 INSERT INTO game (title) VALUES ('Sekiro');
 INSERT INTO game (title) VALUES ('The Legend of Zelda: A Link to the Past');
 INSERT INTO game (title) VALUES ('The Legend of Zelda: Ocarina of Time');
@@ -31,6 +32,35 @@ INSERT INTO game (title) VALUES ('Dark Souls III');
 INSERT INTO game (title) VALUES ('Super Mario 64');
 INSERT INTO game (title) VALUES ('Trine');
 INSERT INTO game (title) VALUES ('Trine 2');
+INSERT INTO game (title) VALUES ('Portal');
 
+/* Inserting Users */
+INSERT INTO users (username) VALUES ('Cadfel');
+INSERT INTO users (username) VALUES ('Distortion2');
+INSERT INTO users (username) VALUES ('LilAggy');
+INSERT INTO users (username) VALUES ('sTaTic_dr0P');
+INSERT INTO users (username) VALUES ('Scooch');
+INSERT INTO users (username) VALUES ('FUSTERCLUCK');
+INSERT INTO users (username) VALUES ('rtyler91');
+INSERT INTO users (username) VALUES ('thefezz');
+INSERT INTO users (username) VALUES ('Shorty Da Moose');
+INSERT INTO users (username) VALUES ('Alexden96');
+INSERT INTO users (username) VALUES ('ScoutATB');
+INSERT INTO users (username) VALUES ('Devilteeth');
+INSERT INTO users (username) VALUES ('Sacaix');
+INSERT INTO users (username) VALUES ('jester1570');
+
+/* Inserting categories*/
 INSERT INTO category (category_title) VALUES ('100%');
 INSERT INTO category (category_title) VALUES ('Any %');
+INSERT INTO category (category_title) VALUES ('All Bosses');
+INSERT INTO category (category_title) VALUES ('Glitchless');
+
+/* Inserting runs*/
+INSERT INTO run (user_id, game_id, platform_id, time, valid, category_id) VALUES (1, 1, 1, '00:21:42', True, 2);
+INSERT INTO run (user_id, game_id, platform_id, time, valid, category_id) VALUES (2, 1, 2, '00:19:55', True, 2);
+INSERT INTO run (user_id, game_id, platform_id, time, valid, category_id) VALUES (4, 4, 6, '01:41:30', True, 1);
+INSERT INTO run (user_id, game_id, platform_id, time, valid, category_id) VALUES (10, 16, 1, '00:16:14', True, 4);
+
+/* Displaying table with | username | Game | Category | Time | Platform | Validated | */
+SELECT users.username, game.title, category.category_title, run.time, platform.name, run.valid FROM users, run, platform, game, category WHERE run.user_id = users.id AND platform_id = platform.id AND run.game_id = game.id AND run.category_id = category.id ORDER BY run.time;
