42dcaf0dfae6503a5148bf006ccecf6d7da04ef4
Josh Elison <joshelison@gmail.com>
2019-10-18 10:35:23 -0600

    fixed generate table

--- web/Speedrun/home.php
+++ web/Speedrun/home.php
@@ -14,7 +14,7 @@
         while ($row = $statement->fetch(PDO::FETCH_ASSOC))
         {
             var_dump($row);
-            echo '<tr><td colspan=6>' . 'does this work?' . $row['username'] . '</td></tr>';
+            echo '<tr><td>'.$row['username'].'</td><td>'.$row['title'].'</td><td>'.$row['category_title'].'</td><td>'.$row['time'].'</td><td>'.$row['name'].'</td><td>'.$row['valid'].'</td></tr>';
         }
     }
 
