d99c80ad05347686187bb4f90521148d4fadb889
Josh Elison <joshelison@gmail.com>
2019-10-18 12:54:45 -0600

    added blank layer for formating

--- web/Speedrun/home.php
+++ web/Speedrun/home.php
@@ -63,7 +63,7 @@
             <td colspan="3"><select id=gameSelect><option value=''>Select a Game</option><?php generateGameDropdown(); ?></select></td>
             <td colspan="3"><select id=runCategory><option value=''>Select a Category</option></select></td>
         </tr>
-
+        <tr></tr>
         <tr><th>User</th><th>Game</th><th>Category</th><th>Time</th><th>Platform</th><th>Validation</th></tr>
         <?php displayTable(); ?>
     </table>
