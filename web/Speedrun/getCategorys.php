3fab563cd8f71fcec93ebd0d9462d02286d97d78
Josh Elison <joshelison@gmail.com>
2019-10-18 17:40:53 -0600

    commented out bottom of code for debugging

--- web/Speedrun/getCategorys.php
+++ web/Speedrun/getCategorys.php
@@ -6,12 +6,12 @@
 
   $gameQuery='SELECT DISTINCT run.category_id, category.category_title FROM run, category WHERE run.category_id = category.id AND run.game_id='.$gameID.';';
   $statement = $db->query($gameQuery);
-
+/*
   $categorys = array();
   while ($row = $statement->fetch(PDO::FETCH_ASSOC))
   {
     $categorys += $row['category_title'];
   }
 
-  echo json_encode($categorys);
+  echo json_encode($categorys);*/
 ?>
