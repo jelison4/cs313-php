bb16b0856add1a818ebaf143003dbab332e71bb4
Josh Elison <joshelison@gmail.com>
2019-10-18 19:05:04 -0600

    afdgasf

--- web/Speedrun/speedrunJava.js
+++ web/Speedrun/speedrunJava.js
@@ -1,32 +1,31 @@
 function generateCatDropdown(){
     var gameID=document.getElementById('gameSelect').value;
-    console.log(gameID);
-    
-    
+    if(gameID!=0){
         var queryString='getCategorys.php?gameID='+gameID;
-        console.log(queryString);
+
 
         var gameID=document.getElementById('gameSelect').value;
-        console.log(gameID);
+
         var queryString='getCategorys.php?gameID='+gameID;
-        console.log(queryString);
+
         var xhr = new XMLHttpRequest();
 
         xhr.onreadystatechange = function(){
             if (xhr.readyState==4 && xhr.status==200){
                 categorys = JSON.parse(xhr.responseText);
-                console.log(categorys);
-//'runCategory'
+
                 var categoryDropdown = '<option value="0">Select a Category</option>';
                 categorys.forEach(element => {
                     categoryDropdown +="<option value=" + element.id  + ">" + element.name + "</option>";
                 });
-                console.log(categoryDropdown);
 
                 document.getElementById('runCategory').innerHTML = categoryDropdown;
             }
         }
         xhr.open("GET", queryString, false);
         xhr.send();
-    
+    }
+    if(gameID==0){
+        document.getElementById('runCategory').innerHTML = '<option value="0">Select a Category</option>';
+    }
 }
