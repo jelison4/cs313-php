6862ac2f20cfa547cafffe497ce9c441207b6891
Josh Elison <joshelison@gmail.com>
2019-10-18 18:17:32 -0600

    tweeked the array adder

--- web/Speedrun/getCategorys.php
+++ web/Speedrun/getCategorys.php
@@ -3,7 +3,7 @@
 
   $game=$_POST['gameID'];
   $db = get_db();
-/*
+
   $gameQuery='SELECT DISTINCT run.category_id, category.category_title FROM run, category WHERE run.category_id = category.id AND run.game_id='.$game.';';
   
   $statement = $db->query($gameQuery);
@@ -13,14 +13,13 @@
   while ($row = $statement->fetch(PDO::FETCH_ASSOC))
   {
     if($count==1){
-      $categorys = array("'category_".$count"'"=>$row['category_title']);
+      $categorys = array("'category_".$count."'"=>$row['category_title']);
     }
     else{
-      $categorys = array("'category_".$count"'"=>$row['category_title']);
+      $categorys.array_push("'category_".$count."'"=>$row['category_title']);
     }
     $count+=1;
   }
-*/
-  $categorys = array("category_1"=>"Any %","category_2"=>"100%");
+
   echo json_encode($categorys);
 ?>
