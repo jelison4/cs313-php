f99ef183e4a15f1a525d89b28c20c86706f99cf4
Josh Elison <joshelison@gmail.com>
2019-10-18 18:28:27 -0600

    sdf

--- web/Speedrun/getCategorys.php
+++ web/Speedrun/getCategorys.php
@@ -7,15 +7,14 @@
   $gameQuery='SELECT DISTINCT run.category_id, category.category_title FROM run, category WHERE run.category_id = category.id AND run.game_id='.$game.';';
  
   $statement = $db->query($gameQuery);
-
-  $count = 1;
+/*
   $categorys = array();
   while ($row = $statement->fetch(PDO::FETCH_ASSOC))
   {
     $categorys.array_push($row['category_id']=>$row['category_title']);
   }
-
- //$categorys = array( 2=>'Any %', 1=>'100%', 4=>'glitchless');
+*/
+ $categorys = array( 2=>'Any %', 1=>'100%', 4=>'glitchless');
 
   echo json_encode($categorys);
 ?>
