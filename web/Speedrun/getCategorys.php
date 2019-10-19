86261d0d12723a4051867b17c9a95df3c3cfe457
Josh Elison <joshelison@gmail.com>
2019-10-18 18:24:36 -0600

    afga

--- web/Speedrun/getCategorys.php
+++ web/Speedrun/getCategorys.php
@@ -1,13 +1,13 @@
 <?php
   require "databaseConnect.php";
 
-  $game=$_POST['gameID'];
+  $game=$_GET['gameID'];
   $db = get_db();
 
   $gameQuery='SELECT DISTINCT run.category_id, category.category_title FROM run, category WHERE run.category_id = category.id AND run.game_id='.$game.';';
-  /*
+ 
   $statement = $db->query($gameQuery);
-
+ /*
   $count = 1;
   $categorys = null;
   while ($row = $statement->fetch(PDO::FETCH_ASSOC))
