31bc916f4ca83ff202ade6e418b2b45d81c02c4f
Josh Elison <joshelison@gmail.com>
2019-10-18 23:26:06 -0600

    asas

--- web/Speedrun/speedrunJava.js
+++ web/Speedrun/speedrunJava.js
@@ -43,10 +43,12 @@ function generateTable(){
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 document.getElementById('runTable').innerHTML = xhr.responseText;
-                console.log(xhr.responseText);
             }
         }
         xhr.open("GET", queryString, true);
         xhr.send();
+    }
+    else{
+        document.getElementById('runTable').innerHTML='';
     }
 }
