e7eaf2c1c57f7f2c205ed1eb2976c8f1b7e7380d
Josh Elison <joshelison@gmail.com>
2019-10-19 18:46:21 -0600

    removed some old tags

--- web/Speedrun/home.php
+++ web/Speedrun/home.php
@@ -24,7 +24,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <header class='full-width-borders'>
+    <header>
       <h1>SPEEDRUN!!!!(Working Title)</h1>
     </header>
 
--- web/Speedrun/submit.php
+++ web/Speedrun/submit.php
@@ -34,7 +34,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <header class='full-width-borders'>
+    <header>
       <h1>Submission</h1>
     </header>
 
