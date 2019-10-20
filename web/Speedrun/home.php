cc120f2e5a51a6e0f2beb251ae01ce76f661b926
Josh Elison <joshelison@gmail.com>
2019-10-19 18:42:51 -0600

    removed extra text

--- web/Speedrun/home.php
+++ web/Speedrun/home.php
@@ -38,9 +38,6 @@
         </tr>
     </table>
     <table id='runTable'></table>
-    
-    'PC''Playstation 4' 'Xbox One' 'Nintendo Switch' 'Nintendo 64' 'Super Nintendo'
-
     <form action='submit.php'><input type="submit"value='Submit Run'/></form>
 
     </div>
