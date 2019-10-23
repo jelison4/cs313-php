1d6d434601a6afc2ac0feb7923de69b2ca982aff
Josh Elison <joshelison@gmail.com>
2019-10-22 20:48:56 -0600

    asdf

--- web/Speedrun/home.php
+++ web/Speedrun/home.php
@@ -1,23 +1,6 @@
-<?php
-function generateGameDropdown(){
-    $db = get_db();
-    $gameQuery='SELECT DISTINCT run.game_id, game.title FROM run, game WHERE run.game_id = game.id ORDER BY game.title';
-    $statement = $db->query($gameQuery);
-
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
-        echo "<option value=" . $row['game_id']  . ">" . $row['title'] . "</option>";
-    }
-}
-
-function generatePlatformDropdown(){
-    $db = get_db();
-    $gameQuery='SELECT * FROM platform';
-    $statement = $db->query($gameQuery);
-
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
-        echo "<option value=" . $row['id']  . ">" . $row['name'] . "</option>";
-    }
-}
+<?php 
+  require 'dropdowns.php';
+  generateGameDropdown();
 ?>
 
 <!DOCTYPE html>
