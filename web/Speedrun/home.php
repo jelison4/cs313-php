4557aefd7b682b3deb7dc881f53a54558f29da64
Josh Elison <joshelison@gmail.com>
2019-10-23 22:21:06 -0600

    sdfsdf

--- web/Speedrun/home.php
+++ web/Speedrun/home.php
@@ -60,6 +60,19 @@
       <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
     </div>
   </form>
+
+  <script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+
 </div>
     
 
