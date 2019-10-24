3a271200e7c71a6c87518e00220a67ea96f19346
Josh Elison <joshelison@gmail.com>
2019-10-23 23:15:03 -0600

    added registration page

---
+++ web/Speedrun/register.php
@@ -0,0 +1,32 @@
+<?php
+  
+?>
+
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+  <meta charset="UTF-8">
+  <title>Speed Running!!</title>
+  <link rel="stylesheet" type="text/css" href="speedrunSS.css">
+  <script src="speedrunJava.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+      <h1>Register</h1>
+    </header>
+
+    <div class='background'>
+        <form>
+            <label for="uname"><b>Username:</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" id='uname' required><br>
+
+            <label for="password1"><b>Password</b></label>
+            <input type="password1" placeholder="Enter Password" name="password" id="password1" required><br>
+
+            <label for="password2"><b>Password</b></label>
+            <input type="password" placeholder="Repeat Password" name="password2" id="password2" required><br>
+        </form>    
+    </div>
+</body>
+</html>
