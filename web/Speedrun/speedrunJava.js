655c04e03870db174859593e886cdb2438f42b1b
Josh Elison <joshelison@gmail.com>
2019-10-23 22:53:27 -0600

    asdad

--- web/Speedrun/speedrunJava.js
+++ web/Speedrun/speedrunJava.js
@@ -61,3 +61,13 @@ function generateTable(){
 function submit(){
     console.log('It works I guess');
 }
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
