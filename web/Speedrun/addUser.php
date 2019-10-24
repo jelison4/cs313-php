de2963d702ca5d7ab96a11268195d436adc95fea
Josh Elison <joshelison@gmail.com>
2019-10-24 13:45:11 -0600

    sadf

--- web/Speedrun/addUser.php
+++ web/Speedrun/addUser.php
@@ -5,7 +5,7 @@
   $name=$_Get['newName'];
   $password=$_GET['password'];
 
-  $query='INSERT INTO users (username, password, admin) VALUES ('.$name.', '.$password.', False);';
+  $query="INSERT INTO users (username, password, admin) VALUES ("."'".$name."'".", '".$password."', False);";
 
   $db->query($query);
 ?>
