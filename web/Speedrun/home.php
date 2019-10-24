a9d757cfaf3e0238a1038d6d4b4a2cff6a0fc3d3
Josh Elison <joshelison@gmail.com>
2019-10-23 23:03:20 -0600

    asdf

--- web/Speedrun/home.php
+++ web/Speedrun/home.php
@@ -37,40 +37,28 @@
           </tr>
       </table>
     </div>
-</div>
 
-<?php
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-?>
 
-<!-- The Modal -->
-<div id="login" class="modal">
-  <span onclick="document.getElementById('login').style.display='none'"
-class="close" title="Close Modal">&times;</span>
-
-  <!-- Modal Content -->
-  <form class="modal-content" action="/action_page.php">
-
-    <div class="container">
-      <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
-
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
-
-      <!--<button type="submit">Login</button>-->
+    <div id="login" class="modal">
+      <span onclick="document.getElementById('login').style.display='none'" class="close" title="Close Modal">&times;</span>
+    
+      <form class="modal-content" action="/action_page.php">
+    
+        <div class="container">
+          <label for="uname"><b>Username</b></label>
+          <input type="text" placeholder="Enter Username" name="uname" id='uname' required>
+    
+          <label for="password"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="password" id="password" required>
+    
+        </div>
+    
+        <div class="container" style="background-color:#f1f1f1">
+          <button type="submit">Login</button>
+          <button type="button" onclick="document.getElementById('login').style.display='none'" class="cancelbtn">Cancel</button>
+        </div>
+      </form>
     </div>
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="submit">Login</button>
-      <button type="button" onclick="document.getElementById('login').style.display='none'" class="cancelbtn">Cancel</button>
-    </div>
-  </form>
-</div>
-
-<?php
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-?>
 
 </body>
 </html>
