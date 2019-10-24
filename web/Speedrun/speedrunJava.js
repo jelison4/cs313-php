951b43487f93818345488930ddb08307cc57be4f
Josh Elison <joshelison@gmail.com>
2019-10-23 22:55:10 -0600

    dgfhsd

--- web/Speedrun/speedrunJava.js
+++ web/Speedrun/speedrunJava.js
@@ -57,17 +57,3 @@ function generateTable(){
         document.getElementById('runTable').innerHTML='';
     }
 }
-
-function submit(){
-    console.log('It works I guess');
-}
-
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
