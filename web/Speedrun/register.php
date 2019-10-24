d1cf3fab7d6fe18a3e2630d8a724d1cbc84ca78b
Josh Elison <joshelison@gmail.com>
2019-10-23 23:33:14 -0600

    formatting

--- web/Speedrun/register.php
+++ web/Speedrun/register.php
@@ -19,9 +19,9 @@
     <div class='background'>
         <form>
             <table>
-                <tr><td><b>Username:</b></td><td><input type="text" placeholder="Enter Username" name="uname" id='uname' required></td></tr>
-                <tr><td><b>Password:</b></td><td><input type="password1" name="password" id="password1" required></td></tr>
-                <tr><td><b>Confirm Password:</b></td><td><input type="password" name="password2" id="password2" required></td></tr>
+                <tr><td class='col1'><b>Username:</b></td><td><input type="text" placeholder="Enter Username" name="uname" id='uname' required></td></tr>
+                <tr><td class='col1'><b>Password:</b></td><td><input type="password1" name="password" id="password1" required></td></tr>
+                <tr><td class='col1'><b>Confirm Password:</b></td><td><input type="password" name="password2" id="password2" required></td></tr>
             </table>
         </form>    
     </div>
