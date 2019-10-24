cec1dfe468b549eaa99719940c3bf414503fca9e
Josh Elison <joshelison@gmail.com>
2019-10-23 22:34:56 -0600

    sdf

--- web/Speedrun/home.php
+++ web/Speedrun/home.php
@@ -36,50 +36,6 @@
           </tr>
       </table>
     </div>
-
-    <?php//<button onclick="document.getElementById('login').style.display='block'" style="width:auto;">Login</button>?>
-
-<div id="login" class="modal">
-  
-  <form action="login.php" method="post">
-     
-  <form class="modal-content" action="/action_page.php" method="post">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('login').style.display='none'" class="close" title="Close Modal">&times;</span>
-    </div>
-
-    <div class="container">
-      <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
-
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
-        
-      <button type="submit">Login</button>
-      <label>
-        <input type="checkbox" checked="checked" name="remember"> Remember me
-      </label>
-    </div>
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('login').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
-    </div>
-  </form>
-</div>
-
-  <script>
-// Get the modal
-var modal = document.getElementById('login');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
-
 </div>
     
 
