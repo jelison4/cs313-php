e64dce70e28d28aa0c1ba0e4c85595eb6530e764
Josh Elison <joshelison@gmail.com>
2019-10-24 12:33:50 -0600

    asdfsdf

--- web/Speedrun/register.php
+++ web/Speedrun/register.php
@@ -17,10 +17,10 @@
     </header>
 
     <div class='background'>
-        <form onsubmit="validate()">
+        <form id='register' onsubmit="validate()">
             <table>
                 <tr><td class='col1'><b>Username:</b></td><td><input type="text" name="uname" id='uname' required></td></tr>
-                <tr><td class='col1'><b>Password:</b></td><td><input type="password1" name="password" id="password1" required></td></tr>
+                <tr><td class='col1'><b>Password:</b></td><td><input type="password" name="password1" id="password1" required></td></tr>
                 <tr><td class='col1'><b>Confirm Password:</b></td><td><input type="password" name="password2" id="password2" required></td></tr>
                 <tr><td colspan="2"><button style="text-align:center" type="submit">Register</button></td></tr>
             </table>
--- web/Speedrun/registerJava.js
+++ web/Speedrun/registerJava.js
@@ -1,8 +1,11 @@
 function validate(){
-    var pass1 = document.getElementById('password1').value;
-    var pass2 = document.getElementById('password2').value;
+    var pass1 = document.getElementById("register").elements[1].value;
+    var pass2 = document.getElementById("register").elements[2].value;
+
+    console.log(pass1);
+    console.log(pass2);
 
     if(pass1!=pass2){
-        
+        document.getElementById("register").elements[2].statusindicator.value='Passwords do not match.';;
     }
 }
