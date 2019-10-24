b2dc41725b654e9839f5f37c0c78ba4ca894ad5a
Josh Elison <joshelison@gmail.com>
2019-10-23 22:03:51 -0600

    sdfsf

--- web/Speedrun/home.php
+++ web/Speedrun/home.php
@@ -31,12 +31,41 @@
       <br>
       <table>
           <tr>
-            <td><button class='col1' onclick='submit.php'>Login</button></td>
-            <td><button class='col2' onclick='submit()'>Register</button></td>
+            <td><button class='col1' onclick=''>Login</button></td>
+            <td><button class='col2' onclick=''>Register</button></td>
           </tr>
       </table>
     </div>
+//////////////////////////////////////////////////////////////////////////////
+    <button onclick="document.getElementById('login').style.display='block'" style="width:auto;">Login</button>
 
+<div id="login" class="modal">
+  
+  <form action="login.php" method="post">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <img src="img_avatar2.png" alt="Avatar" class="avatar">
+    </div>
+
+    <div class="container">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+        
+      <button type="submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+  </form>
+</div>
     
 
 </body>
