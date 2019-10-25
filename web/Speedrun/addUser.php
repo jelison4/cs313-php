f3fcb6439f295a3ee5ac32be8fdb37d7024731be
Josh Elison <joshelison@gmail.com>
2019-10-24 18:17:28 -0600

    h

--- web/Speedrun/addUser.php
+++ web/Speedrun/addUser.php
@@ -1,4 +1,5 @@
 <?php
+  function addUser(){
     require 'databaseConnect.php';
     $db=get_db();
   
@@ -20,15 +21,35 @@
         echo "Sorry that username is taken.";
     }
     else{
-        $name=$_POST['uname'];
         // Hash password
         $password=$_POST['password'];
         $hash = password_hash($password, PASSWORD_DEFAULT);
 
-        // Add new user to database
+        // Write user to database
         $query="INSERT INTO users (username, password, admin) VALUES ("."'".$name."'".", '".$hash."', False);";
         $db->query($query);
 
         echo "Account created";
     }
+  }
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+  <meta charset="UTF-8">
+  <title>Speed Running!!</title>
+  <link rel="stylesheet" type="text/css" href="speedrunSS.css">
+  <script src="speedrunJava.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+        <h1>Working title</h2>
+    </header>
+
+    <div class=background><?php addUser();?></div>
+
+</body>
+</html>
