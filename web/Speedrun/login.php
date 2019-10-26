eec012b882efdbdbcb450cc239d09afaa958c727
Josh Elison <joshelison@gmail.com>
2019-10-26 15:12:04 -0600

    sfsadfsaf

--- web/Speedrun/login.php
+++ web/Speedrun/login.php
@@ -4,11 +4,12 @@
     $name=$_GET['uname'];
     $password=$_GET['password'];
     $passQuery=$db->query("SELECT password FROM users WHERE username='".$name."'".";");
-    $hashPass=$passQuery->fetch(PDO::FETCH_ASSOC);
+    //$hashPass=$passQuery->fetch(PDO::FETCH_ASSOC);
 /*
     if(password_verify($password, $hashPass['password'])){
         $_SESSION['uname'] = $name;
         echo 1;
     }
 */
+    echo 1;
 ?>
