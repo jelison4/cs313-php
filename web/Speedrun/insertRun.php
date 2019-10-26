2ac8ac71d89ff2eaccdaf0454e75f804f8521517
Josh Elison <joshelison@gmail.com>
2019-10-26 16:00:42 -0600

    sefwef

--- web/Speedrun/insertRun.php
+++ web/Speedrun/insertRun.php
@@ -1,11 +1,11 @@
 <?php
   require 'databaseConnect.php';
 
-  $user_id=$_GET['user_id'];
-  $game_id=$_GET['game_id'];
-  $plat_id=$_GET['plat_id'];
-  $time=$_GET['time'];
-  $cat_id=$_GET['cat_id'];
+  $user_id=$_POST['user_id'];
+  $game_id=$_POST['game_id'];
+  $plat_id=$_POST['plat_id'];
+  $time=$_POST['time'];
+  $cat_id=$_POST['cat_id'];
 
   $query="INSERT INTO run (user_id, game_id, platform_id, time, valid, category_id) VALUES ($user_id, $game_id, $plat_id, $time, 0, $cat_id);";
 
--- web/Speedrun/submitJava.js
+++ web/Speedrun/submitJava.js
@@ -7,13 +7,50 @@ function databaseWrite() {
 
     var queryString = `insertRun.php?user_id=${user_id}&game_id=${game_id}&plat_id=${plat_id}&time=${time}&cat_id=${cat_id}`;
     console.log(queryString);
-    /*
+    
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {}
         
     }
-    xhr.open("GET", queryString, false);
+    xhr.open("POST", queryString, false);
     xhr.send();
-    */
+}
+
+function generateCatDropdown(){
+    var gameID = document.getElementById('gameSelect').value;
+    if (gameID != 0) {
+        var queryString = 'getCategorys.php?gameID=' + gameID;
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                categorys = JSON.parse(xhr.responseText);
+
+                var categoryDropdown = null;
+                if(document.getElementById('runTable') != null){
+                    var categoryDropdown = "<option value=category.id>All</option>";
+                }
+                
+                categorys.forEach(element => {
+                    if(categoryDropdown==null){
+                        categoryDropdown = "<option value=" + element.id + ">" + element.name + "</option>";
+                    }
+                    else{
+                        categoryDropdown += "<option value=" + element.id + ">" + element.name + "</option>";
+                    }
+                });
+
+                document.getElementById('runCategory').innerHTML = categoryDropdown;
+                generateTable();
+            }
+        }
+        xhr.open("GET", queryString, false);
+        xhr.send();
+    }
+    if (gameID == 0 && document.getElementById('runTable') != null) {
+        document.getElementById('runTable').innerHTML='';
+        document.getElementById('runCategory').innerHTML = '<option value="0">-</option>';
+    }
 }
