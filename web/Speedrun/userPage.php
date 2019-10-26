f9da1a661987dca9f0152047b57903d9b44461e0
Josh Elison <joshelison@gmail.com>
2019-10-26 16:52:16 -0600

    sdf

--- web/Speedrun/userPage.php
+++ web/Speedrun/userPage.php
@@ -8,19 +8,22 @@
     $tableQuery="SELECT DISTINCT game.title, run.time, category.category_title, platform.name, run.valid FROM users, run, platform, category, game WHERE run.user_id = users.id AND users.username="."'".$_SESSION['uname']."'"." AND platform_id = platform.id AND run.game_id = game.id AND run.category_id = category.id ORDER BY run.time;";
     $table=$db->query($tableQuery);
     
-    if($table->fetch(PDO::FETCH_ASSOC) == null){
+    if(mysql_num_rows($table) == 0){
       echo '<tr><th>'.$_SESSION['uname'].'has no submitions</th><tr>';
     }
 
-    $title=0;
-    while ($row = $table->fetch(PDO::FETCH_ASSOC)){
-      if($title==0){
+    else{
+      $title=0;
+      while ($row = $table->fetch(PDO::FETCH_ASSOC)){
+        if($title==0){
           echo '<tr><th colspan=5><h2>'.$_SESSION['uname']. "'s Submissions</h2></th></tr>";
           echo '<tr><th>Game</th><th>Time</th><th>Category</th><th>Platform</th><th>Validity</th></tr>';
           $title=1;
+        }
+        echo '<tr><td>'.$row['title'].'</td><td>'.formatTime($row['time']).'</td><td>'.$row['category_title'].'</td><td>'.$row['name'].'</td>'.valitity($row['valid']).'</td></tr>';
       }
-      echo '<tr><td>'.$row['title'].'</td><td>'.formatTime($row['time']).'</td><td>'.$row['category_title'].'</td><td>'.$row['name'].'</td>'.valitity($row['valid']).'</td></tr>';
     }
+    
   }
   // Trims the leading 0's and :'s
   function formatTime($time){
