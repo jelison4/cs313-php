b9465d93567bc0226f2b391d80d6ebe2e4de0f41
Josh Elison <joshelison@gmail.com>
2019-10-26 19:10:20 -0600

    I hope this works

--- web/Speedrun/insertRun.php
+++ web/Speedrun/insertRun.php
@@ -1,7 +1,9 @@
 <?php
   require 'databaseConnect.php';
 
-  $user_id=$_POST['user_id'];
+  $db=get_db();
+
+  $user_id=getUserID();
   $game_id=$_POST['game_id'];
   $plat_id=$_POST['plat_id'];
   $time=$_POST['time'];
@@ -9,7 +11,22 @@
 
   $query="INSERT INTO run (user_id, game_id, platform_id, time, valid, category_id) VALUES ($user_id, $game_id, $plat_id, $time, 0, $cat_id);";
 
-  $db=get_db();
+  $status=$db->query($query);
 
-  $db->query($query);
+  if($status){
+    echo "Update sucessfull.";
+  }
+  else{
+    echo "Something got fucked up.";
+  }
+
+  function getUserID(){
+    $idQuery="SELECT id FROM users WHERE username="."'".$_SESSION['uname']."';";
+    $db=get_db();
+
+    $statement = $db->query($idQuery);
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+    
+    return $row['id'];
+  }
 ?>
--- web/Speedrun/submitJava.js
+++ web/Speedrun/submitJava.js
@@ -1,17 +1,17 @@
 function databaseWrite() {
-    var user_id = document.getElementById('username').value;
     var game_id = document.getElementById('gameSelect').value;
     var plat_id = document.getElementById('platform').value;
     var time = document.getElementById('time').value;
     var cat_id = document.getElementById('runCategory').value;
 
-    var queryString = `insertRun.php?user_id=${user_id}&game_id=${game_id}&plat_id=${plat_id}&time=${time}&cat_id=${cat_id}`;
+    var queryString = "insertRun.php?game_id="+game_id+"&plat_id="+plat_id+"&time="+time+"&cat_id="+cat_id;
     console.log(queryString);
     
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {}
-        
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log(xhr.responseText);
+        }
     }
     xhr.open("POST", queryString, false);
     xhr.send();
