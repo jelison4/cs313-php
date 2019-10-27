e8af502adfdf5d34f77c70b84ba80564be8baff6
Josh Elison <joshelison@gmail.com>
2019-10-26 18:17:48 -0600

    adfs

--- web/Speedrun/dropdowns.php
+++ web/Speedrun/dropdowns.php
@@ -3,6 +3,17 @@
 function generateGameDropdown(){
     require 'databaseConnect.php';
     $db = get_db();
+    //$gameQuery='SELECT DISTINCT run.game_id, game.title FROM run, game WHERE run.game_id = game.id ORDER BY game.title;';
+    $statement = $db->query('SELECT DISTINCT run.game_id, game.title FROM run, game WHERE run.game_id = game.id ORDER BY game.title;');
+
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+        echo "<option value=" . $row['game_id']  . ">" . $row['title'] . "</option>";
+    }
+}
+
+function generateCategoryDropdown(){
+    require 'databaseConnect.php';
+    $db = get_db();
     $gameQuery='SELECT DISTINCT run.game_id, game.title FROM run, game WHERE run.game_id = game.id ORDER BY game.title;';
     $statement = $db->query($gameQuery);
 
