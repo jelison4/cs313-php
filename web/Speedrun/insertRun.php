c236ecfeebe57434e37ba8e868048d5261920c8b
Josh Elison <joshelison@gmail.com>
2019-11-02 20:25:46 -0600

    sdf

--- web/Speedrun/insertRun.php
+++ web/Speedrun/insertRun.php
@@ -6,7 +6,7 @@
   $user_id=getUserID();
   $game_id=$_POST['game_id'];
   $plat_id=$_POST['plat_id'];
-  $time=formatTime($_POST['time']);
+  $time=$_POST['time'];
   $cat_id=$_POST['cat_id'];
     
   $insertQuery="INSERT INTO run (user_id, game_id, platform_id, time, valid, category_id) VALUES ($user_id, $game_id, $plat_id,"."'". $time."'".", false, $cat_id);";
@@ -22,21 +22,6 @@
    
     return $row['id'];
   }
-
-  function formatTime($time){
-    if(strlen($time)=2){
-      $time='00:00:'+$time;
-    }
-    if(strlen($time)=4){
-      $time='00:0'+$time;
-    }
-    if(strlen($time)=5){
-      $time='00:'+$time;
-    }
-    if(strlen($time)=7){
-      $time='0'+$time;
-    }
-  }
-
+  
   header("Location: userPage.php");
 ?>
