01a040834b161aba59f74adc9639d3beea6d89a3
Josh Elison <joshelison@gmail.com>
2019-11-02 19:48:28 -0600

    sadf

--- web/Speedrun/addUser.php
+++ web/Speedrun/addUser.php
@@ -8,7 +8,7 @@
     $password=$_POST['password'];
     $hash = password_hash($password, PASSWORD_DEFAULT);
 
-    $newUser="INSERT INTO users (username, password, admin) VALUES ('$name', '$hash', True);";
+    $newUser="INSERT INTO users (username, password, admin) VALUES ('$name', '$hash', FALSE);";
     $db->query($newUser);
 
     header("Location: home.php");
