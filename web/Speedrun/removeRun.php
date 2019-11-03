4b5e50628b1b3dc3b3e83aab135d50506b46967d
Josh Elison <joshelison@gmail.com>
2019-11-02 18:21:30 -0600

    sdf

--- web/Speedrun/removeRun.php
+++ web/Speedrun/removeRun.php
@@ -5,12 +5,5 @@
     $runID=$_GET['runID'];
     $sql="DELETE FROM users WHERE id=".$runID.";";
 
-    echo $sql;
-
-    if($db->query($sql)){
-        echo "Run deleted.";
-    }
-    else{
-        echo "Error deleting record: " . $db->error;
-    }
+    $db->query($sql)
 ?>
